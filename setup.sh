fc6de70ffc35e0a4efea263c1b7cefecee9348eb
Leah Alpert <leah.alpert@gmail.com>
2015-02-04 00:40:11 -0800

    Add casperjs setup

--- setup.sh
+++ setup.sh
@@ -4,15 +4,17 @@
 # It clones the git repos, downloads needed packages, and sets up the database.
 # Run script with sudo. Script will prompt for MySQL password.
 
-# TODO: install casperjs
-
-# Get newest version of packages and install git, pip, and phanom
+# Get newest version of packages and install git and pip
 apt-get update
 apt-get install git
 apt-get install python-pip
-apt-get install phantomjs
 
-# Clone public and private repos (private will fail if you don't have access)
+# Install casperjs and phantomjs for the crossword scraping
+apt-get install phantomjs
+git clone git://github.com/n1k0/casperjs.git
+ln -sf `pwd`/casperjs/bin/casperjs /usr/local/bin/casperjs
+
+# Clone public and private smarthouse repos (private will fail if you don't have access)
 git clone https://github.com/lalpert/smarthouse-public.git
 git clone https://github.com/lalpert/smarthouse-private.git
 
